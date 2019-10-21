03a421577a1cfa9b21eae688bf78d52c038af571
GuyCalloway <guyfcalloway@gmail.com>
2019-10-21 18:12:01 +0100

    Third test passing

--- lib/damage_or_sunk.rb
+++ lib/damage_or_sunk.rb
@@ -1,18 +1,16 @@
 def damage_or_sunk(board, attacks)
     results = { 'sunk' => 0, 'damaged' => 0 , 'not_touched' => 1, 'points' => 0 }
     board.each_with_index { |x, i| 
-    
-    
-         if (i == 4 - attacks[0][1]) && (x[attacks[0][0]-1] != 0)
-                                     results['sunk'] += 1
-                                     results['points'] += 1
-                                     results['not_touched'] -= 1
-                                  end
-                                       
-    }
+          attacks.each {|attack| if (i == 4 - attack[1]) && (x[attack[0]-1] != 0)
+            results['sunk'] += 1
+            results['points'] += 1
+            results['not_touched'] -= 1
+         end }
+        }
 
     results
 end
+
 
 
 # is hit method
--- spec/battleship_spec.rb
+++ spec/battleship_spec.rb
@@ -1,7 +1,7 @@
 require 'damage_or_sunk.rb'
 
 describe "battle ship" do
-    it "takes board and attack commands" do
+    it "takes board and single attack command" do
         attacks = [[1, 1]]
         board = [[1,0,0,0,0,0],
         [0,0,0,0,0,0],
@@ -10,7 +10,16 @@ describe "battle ship" do
         expect(damage_or_sunk(board, attacks)).to eq({'sunk' => 0, 'damaged' => 0, 'not_touched' => 1, 'points' => 0})
     end
 
-    it "takes board and attack commands" do
+    it "takes board and single attack command and registers attack" do
+        attacks = [[1, 1]]
+        board = [[0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [1,0,0,0,0,0]]
+        expect(damage_or_sunk(board, attacks)).to eq({'sunk' => 1, 'damaged' => 0, 'not_touched' => 0, 'points' => 1})
+    end
+
+    it "takes board and registers multiple attack commands" do
         attacks = [[1, 1]]
         board = [[0,0,0,0,0,0],
         [0,0,0,0,0,0],
