96d0162fcf7a71b326c84cf2ce37eb7fffda8040
GuyCalloway <guyfcalloway@gmail.com>
2019-10-21 18:17:03 +0100

    Fourth test passing

--- lib/damage_or_sunk.rb
+++ lib/damage_or_sunk.rb
@@ -4,13 +4,18 @@ def damage_or_sunk(board, attacks)
           attacks.each {|attack| if (i == 4 - attack[1]) && (x[attack[0]-1] != 0)
             results['sunk'] += 1
             results['points'] += 1
+            if results['not_touched'] != 0
             results['not_touched'] -= 1
+            end
          end }
         }
 
     results
 end
 
+def is_a_hit(board, attacks, result)
+    
+end
 
 
 # is hit method
--- spec/battleship_spec.rb
+++ spec/battleship_spec.rb
@@ -20,12 +20,21 @@ describe "battle ship" do
     end
 
     it "takes board and registers multiple attack commands" do
-        attacks = [[1, 1], [2, 0]]
+        attacks = [[1, 1], [2, 1]]
         board = [[0,0,0,0,0,0],
         [0,0,0,0,0,0],
         [0,0,0,0,0,0],
         [1,0,0,0,0,0]]
         expect(damage_or_sunk(board, attacks)).to eq({'sunk' => 1, 'damaged' => 0, 'not_touched' => 0, 'points' => 1})
+    end
+
+    it "takes board and registers multiple attack commands" do
+        attacks = [[1, 1], [2, 1]]
+        board = [[0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [1,1,0,0,0,0]]
+        expect(damage_or_sunk(board, attacks)).to eq({'sunk' => 2, 'damaged' => 0, 'not_touched' => 0, 'points' => 2})
     end
 end
 
